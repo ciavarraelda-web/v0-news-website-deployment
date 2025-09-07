67c339cadcc8e6bc6846ab3320425e06563d7674
ciavarraelda-web <ciavarraelda@gmail.com>
2025-09-07 22:52:12 +0200

    Create onesignal-sdk-loader.js

---
+++ public/onesignal-sdk-loader.js
@@ -0,0 +1,9 @@
+<script>
+  // replace YOUR_ONESIGNAL_APP_ID with your real OneSignal App Id in env
+  (function() {
+    var s = document.createElement('script');
+    s.src = 'https://cdn.onesignal.com/sdks/OneSignalSDK.js';
+    s.async = true;
+    document.head.appendChild(s);
+  })();
+</script>
